e7b9a49b53d8458132c760e644b48f6becb3615c
Bill <clarinet500@gmail.com>
2017-08-06 14:22:29 -0700

    Add main menu functionality

--- ratingstrading.src/com/crcapital/frontend/MainMenu.py
+++ ratingstrading.src/com/crcapital/frontend/MainMenu.py
@@ -28,4 +28,3 @@ user_price_per_position = input("What is the price per position?")
 user_number_positions = input("How many positions would you like to buy?")
 
 Trade(user_security,user_price_per_position,user_number_positions).place_trade()
-
