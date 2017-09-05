74d067d7337a2eda15fd65aafd3eb307ac1df8bb
Cristian Gonzales <xcristian.gonzales@gmail.com>
2017-09-04 17:43:16 -0700

    Introduce BeautifulSoup to parse HTML

--- ratingstrading.src/com/crcapital/ratingscrawler/ratingscrawler/spiders/CRCRatingsMoodys.py
+++ ratingstrading.src/com/crcapital/ratingscrawler/ratingscrawler/spiders/CRCRatingsMoodys.py
@@ -8,6 +8,7 @@ import logging
 import scrapy
 
 import threading
+from bs4 import BeautifulSoup
 
 from com.crcapital.main import CRCRatingsWebdriver
 from com.crcapital.ratingscrawler.ratingscrawler.CRCRatingsPair import CRCRatingsPair
@@ -35,42 +36,68 @@ class CRCRatingsMoodys(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    # Handle response downloaded for each request made
+    """
+        Handle response downloaded for each request made
+        :param response: response from Scrapy scraping
+        :return: void
+    """
     def parse(self, response):
-        # Declaring the names, statuses, and titles lists
-        comp_names = response.xpath(
-            '//*[contains(concat( " ", @class, " " ), concat( " ", "mdcResearchDocLink", " " ))]'
-        ).extract()
-        logging.debug("names length: " + str(len(comp_names)))
+        try:
+            # Declaring the names, statuses, and titles lists
+            comp_names = response.xpath(
+                '//*[contains(concat( " ", @class, " " ), concat( " ", "mdcResearchDocLink", " " ))]'
+            ).extract()
+            logging.debug("names length: " + str(len(comp_names)))
 
-        # Commented out because it was inconsistent with the length of the names array
-        # statuses = response.css(
-        #     '.mdcResearchDocTypeValue~ td+ td a'
-        # ).extract()
-        credit_statuses = response.css(
-            '.mdcResearchDocTypeValue~ td+ td'
-        ).extract()
-        logging.debug("statuses length: " + str(len(credit_statuses)))
+            # Commented out because it was inconsistent with the length of the names array
+            # credit_statuses = response.css(
+            #     '.mdcResearchDocTypeValue~ td+ td a'
+            # ).extract()
+            credit_statuses = response.css(
+                '.mdcResearchDocTypeValue~ td+ td'
+            ).extract()
+            logging.debug("statuses length: " + str(len(credit_statuses)))
 
-        # Start a new thread to actually parse the data
-        threading.Thread(target=self.data_storing(comp_names, credit_statuses)).start()
+            # Start a new thread to actually parse the data
+            threading.Thread(target=self.data_storing(comp_names, credit_statuses)).start()
+        except Exception as e:
+            logging.error(e)
 
+    """
+        New thread to parse the data as HTML and store inside ADT
+        :param names: The names as HTML from Scrapy webscraping
+        :param statuses: The statuses as HTML from Scrapy webscraping
+        :return: void
+    """
     def data_storing(self, names, statuses):
         # Declaring the pair object to add to the global list of key-value pairs
         pairObj = CRCRatingsPair()
 
+        # Storing each item in ADT (to be stored in an array of ADTs)
         for i in range(len(names)):
             try:
-                # Debugging
-                logging.debug("Company name: " + names[i])
-                logging.debug("Credit status: " + statuses[i])
-                # Setting the names
-                pairObj.set_name(names[i])
+                # logging.debug("Company name (HTML): " + names[i])
+                # logging.debug("Credit status (HTML): " + statuses[i])
+
+                # Setting the names using BeautifulSoup
+                name_soup = BeautifulSoup(names[i], "lxml")
+                name = name_soup.a.string
+                logging.debug(name)
+                pairObj.set_name(name)
                 logging.debug("ADT name: " + pairObj.get_name())
-                # Setting the status
-                pairObj.set_status(statuses[i])
+
+                # Setting the status using BeautifulSoup
+                status_soup = BeautifulSoup(statuses[i], "lxml")
+                status = status_soup.a.string
+                logging.debug(status)
+                pairObj.set_status(status)
                 logging.debug("ADT status: " + pairObj.get_status())
-                # Adding the obj to the list
-                pairsList.append(pairObj)
+
+                # Quick hack around ellipses is to just skip it for simplicity. This is something that can be
+                # explored in the future. For now, if the name contains an ellipses, then the object it is
+                # associated with is not added to the list.
+                if "..." not in name:
+                    # Adding the obj to the list
+                    self.pairsList.append(pairObj)
             except Exception as e:
                 logging.error(e)
